cd18ae63b3273267249f5539a61e0092a54c3abb
Adam Kalman <akalman@daptiv.com>
2017-08-22 14:15:44 -0700

    wire up inline theme

--- docs/src/modules/components/AppWrapper.js
+++ docs/src/modules/components/AppWrapper.js
@@ -10,9 +10,146 @@ import AppFrame from 'docs/src/modules/components/AppFrame';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import createMuiTheme from 'material-ui/styles/theme';
 import createPalette from 'material-ui/styles/palette';
-import blue from 'material-ui/colors/blue';
-import pink from 'material-ui/colors/pink';
-import { lightTheme, darkTheme, setPrismTheme } from 'docs/src/modules/utils/prism';
+import { setPrismTheme } from 'docs/src/modules/utils/prism'
+
+const palette = createPalette({
+  primary: {
+    50: '#edf0f5',
+    100: '#dae1ea',
+    200: '#c9d2e0',
+    300: '#92a5c0',
+    400: '#5b77a0',
+    500: '#1c427b',
+    600: '#193b6e',
+    700: '#163461',
+    800: '#132d54',
+    900: '#102647',
+    A100: '#d6ebf7',
+    A200: '#c2e2f4',
+    A400: '#47a7dc',
+    A700: '#0076b5',
+    contrastDefaultColor: 'light'
+  },
+  accent: {
+    50: '#fefcf8',
+    100: '#fdf9f0',
+    200: '#fcf6e9',
+    300: '#faedd3',
+    400: '#f6e3bc',
+    500: '#f3d8a2',
+    600: '#e6cc9a',
+    700: '#d9c191',
+    800: '#ccb689',
+    900: '#bfaa80',
+    A100: '#f0d2a2',
+    A200: '#dfa13c',
+    A400: '#c48d35',
+    A700: '#ab7b2e',
+    contrastDefaultColor: 'dark'
+  }
+})
+
+const fontFamily = '"museo-sans", Arial, Helvetica, sans-serif'
+const fontWeightLight = 300
+const fontWeightRegular = 300
+const fontWeightMedium = 500
+const theme = createMuiTheme({
+  palette: palette,
+  typography: {
+    fontFamily: fontFamily,
+    fontSize: 14,
+    fontWeightLight: fontWeightLight,
+    fontWeightRegular: fontWeightRegular,
+    fontWeightMedium: fontWeightMedium,
+    display4: {
+      fontSize: 112,
+      fontWeight: fontWeightLight,
+      fontFamily,
+      letterSpacing: '-.04em',
+      lineHeight: 1,
+      color: palette.text.secondary,
+    },
+    display3: {
+      fontSize: 56,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      letterSpacing: '-.02em',
+      lineHeight: 1.35,
+      color: palette.text.secondary,
+    },
+    display2: {
+      fontSize: 45,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: '48px',
+      color: palette.text.secondary,
+    },
+    display1: {
+      fontSize: 34,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: '40px',
+      color: palette.text.secondary,
+    },
+    headline: {
+      fontSize: 24,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: '32px',
+      color: palette.text.primary,
+    },
+    title: {
+      fontSize: 21,
+      fontWeight: fontWeightMedium,
+      fontFamily,
+      lineHeight: 1,
+      color: palette.text.primary,
+    },
+    subheading: {
+      fontSize: 15,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: '22px',
+      color: palette.text.primary,
+    },
+    body2: {
+      fontSize: 14,
+      fontWeight: fontWeightMedium,
+      fontFamily,
+      lineHeight: '24px',
+      color: palette.text.primary,
+    },
+    body1: {
+      fontSize: 14,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: '20px',
+      color: palette.text.primary,
+    },
+    caption: {
+      fontSize: 12,
+      fontWeight: fontWeightRegular,
+      fontFamily,
+      lineHeight: 1,
+      color: palette.text.secondary,
+    },
+    button: {
+      fontSize: 14,
+      textTransform: 'uppercase',
+      fontWeight: fontWeightMedium,
+      fontFamily,
+    },
+  },
+  overrides: {
+
+  },
+  // shadows
+  // transitions
+  // mixins
+  // spacing
+  // breakpoints
+  // zIndex
+})
 
 // Injected the insertion-point-jss after docssearch
 if (process.browser && !global.__INSERTION_POINT__) {
@@ -36,30 +173,6 @@ class AppWrapper extends Component {
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-
-    if (this.props.dark) {
-      setPrismTheme(darkTheme);
-    } else {
-      setPrismTheme(lightTheme);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.dark !== this.props.dark) {
-      this.styleContext.theme = createMuiTheme({
-        palette: createPalette({
-          primary: blue,
-          accent: pink,
-          type: nextProps.dark ? 'dark' : 'light',
-        }),
-      });
-
-      if (nextProps.dark) {
-        setPrismTheme(darkTheme);
-      } else {
-        setPrismTheme(lightTheme);
-      }
-    }
   }
 
   styleContext = null;
@@ -70,7 +183,7 @@ class AppWrapper extends Component {
     return (
       <JssProvider registry={this.styleContext.sheetsRegistry} jss={this.styleContext.jss}>
         <MuiThemeProvider
-          theme={this.styleContext.theme}
+          theme={theme}
           sheetsManager={this.styleContext.sheetsManager}
         >
           <AppFrame>
